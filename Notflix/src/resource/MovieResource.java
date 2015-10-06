5c7399806aed83ae9a16fde13746d3eb3094f132
AC2222 <aukecrooijmans@gmail.com>
2015-10-06 12:07:58 +0200

    movies resp

--- Notflix/src/resource/MovieResource.java
+++ Notflix/src/resource/MovieResource.java
@@ -9,33 +9,33 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import model.Gebruiker;
 import model.Model;
 import model.Movie;
 
 @Path("movies")
 public class MovieResource {
 	@Context ServletContext context;
-	private Model model = (Model) context.getAttribute("model");
 	
 	public MovieResource() {
 		System.out.println(context);
 	}
 	
 	@GET
-	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-	public ArrayList<Movie> getMovies() {
-		if(model != null){
-			System.out.println("geen model");
-			return model.getMovies();
-		}
-		return new ArrayList<>();
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public Response getMovies() {
+		Model model = (Model) context.getAttribute("model");
+		return Response.ok().entity(model.getMovies()).build();
+		
 	}
 	
 	@GET
 	@Path("{id}")
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	public Movie getMovie(@PathParam("id") int id){
+		Model model = (Model) context.getAttribute("model");
 		return model.getMovie(id);
 	}
 	
