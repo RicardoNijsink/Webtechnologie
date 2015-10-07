e3a91a4866aa9166634a92136e6d56a1b65e51f3
RicardoNijsink <ricardonijsink@gmail.com>
2015-10-07 15:30:39 +0200

    Post gebruikers

--- Notflix/src/resource/GebruikerResource.java
+++ Notflix/src/resource/GebruikerResource.java
@@ -1,13 +1,21 @@
 package resource;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.fasterxml.jackson.databind.util.JSONPObject;
 
 import model.Gebruiker;
 import model.Model;
@@ -50,6 +58,26 @@ public class GebruikerResource {
 			return Response.ok().entity(model.getGebruiker(nickname)).build();
 		}
 		return Response.noContent().build();
+	}
+	
+	/**
+	 * Methode voor het toevoegen van een gebruiker aan de database
+	 * @param request De verstuurde request
+	 * @param input
+	 * @return Een 200-response met daarin een JSONObject met de aangemaakte gebruiker. Anders foutcode TODO
+	 */
+	@POST
+	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Response postGebruiker(@Context HttpServletRequest request, byte[] input) {
+		Model model = (Model) context.getAttribute("model"); 
+		
+		JSONObject json = new JSONObject();
+		json = new JSONObject(input);
+		
+		System.out.println(json);
+		
+		return Response.ok().entity(model.getGebruiker("test")).build();
 	}
 
 }
