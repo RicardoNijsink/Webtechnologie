ada0b6f595e5e0dc2d026dc1bec5cc56bef2fa59
RicardoNijsink <ricardonijsink@gmail.com>
2015-10-26 13:42:49 +0100

    Pagina's veranderd

--- Notflix/WebContent/login.js
+++ Notflix/WebContent/login.js
@@ -5,6 +5,20 @@ $(document).ready(function(){
 	if(localStorage.getItem("token").length > 0){
 		$("#loginForm").hide();
 		$("#usernav").show();
+		
+		$(".gebruikersRow").append(
+				'<div class="col-lg-4">'
+	  			+ '<p class="text-center">'
+	  				+ '<span class="glyphicon glyphicon-user icon-size"></span>'
+				+ '</p>'
+			    + '</div>'
+			    + '<div class="col-lg-8">'
+			    	+ '<p class="text-left"><strong id="loggedInUserFullName">...</strong></p>'
+			    	+ '<p class="text-left small" id="loggedInUserEmail">...</p>'
+			    	+ '<p class="text-left">'
+			    		+ '<a href="#" class="btn btn-primary btn-block btn-sm">...</a>'
+			    	+ '</p>'
+			    + '</div>');
 	}
 	else{
 		$("#loginForm").show();
